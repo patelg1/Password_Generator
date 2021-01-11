acad9d595a7b30967a018eee7a37b6d66a1f000c
Gopal Patel <patelgopalk1@gmail.com>
2021-01-10 19:17:46 -0500

    Added comments to script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Arrays containing the different criteria for password
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -19,12 +19,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// Function to generate password
 function generatePassword(){
   var passwordHolder = "";
-
+  // Prompt asking user how many characters password should contain
   var passLength = parseInt(prompt("How many characters would you like your password to contain? Choose between 8 and 128"));
-  
+  // Checking to make sure user enters a number
   while(!parseInt(passLength) || passLength < 8 || passLength > 128){
     if (!parseInt(passLength)){
       passLength = ("Invalid input! Please enter a number"); 
@@ -33,12 +33,12 @@ function generatePassword(){
     }  
   }
 
-  
+  // Questions to confirm which criteria user wants to include in password
    var confirmUpperCase = confirm("Do you want upper case letters in password?");
    var confirmLowerCase = confirm("Do you want lower case letters in password?");
    var confirmNumeric = confirm("Do you want numbers in password?");
    var confirmSpecialChar = confirm("Do you want special characters in password?");
-  
+  // Checking to make sure at least one criteria is chosen
   while (!confirmUpperCase && !confirmLowerCase && !confirmNumeric && !confirmSpecialChar){
     alert("Invalid input! You must choose at least one criteria");
    confirmUpperCase = confirm("Do you want upper case letters in password?");
@@ -46,6 +46,7 @@ function generatePassword(){
    confirmNumeric = confirm("Do you want numbers in password?");
    confirmSpecialChar = confirm("Do you want special characters in password?");
   }
+  // Conditional checks to see which criteria user chose
   if (confirmUpperCase === true){
     passChoices = passChoices.concat(upperCase);    
   }
@@ -58,7 +59,7 @@ function generatePassword(){
   if (confirmSpecialChar === true){
     passChoices = passChoices.concat(specialChar);
   }
-
+  // Loop that takes the user's chosen criteria and randomly selects characters
   for (var i = 0; i < passLength; i++){
     passwordHolder = passwordHolder + passChoices[Math.floor(Math.random() * passChoices.length)];
         
